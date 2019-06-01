9c09cba3a9b45e6a7de12db6f45b13a0fc989eda
boyone <boyone.g@gmail.com>
2019-06-01 15:42:37 +0700

    Add RoyaltyTest.

---
+++ java-mvn/src/test/java/RoyaltyTest.java
@@ -0,0 +1,18 @@
+package wlb;
+
+import org.junit.Test;
+
+public class RoyaltyTest {
+    @Test
+    public void receiveTotalPrice239_90ShouldGetNormalPoints23() {
+        // arrange
+        int expectedNormalPoints = 23;
+        Royalty royalty = new Royalty();
+
+        // action
+        int actualNormalPoints = royalty.calculate(239.90);
+
+        // assert
+        assertEqual(expectedNormalPoints, actualNormalPoints);
+    }
+}
